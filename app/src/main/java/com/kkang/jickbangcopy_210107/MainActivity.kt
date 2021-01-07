ef4552b43afae9a1528d1ae8bbd3f55f0e493925
kkangyun <kkang_yj36@naver.com>
2021-01-07 17:17:43 +0900

    표시될 방 목록 강제 추가

--- app/src/main/java/com/kkang/jickbangcopy_210107/MainActivity.kt
+++ app/src/main/java/com/kkang/jickbangcopy_210107/MainActivity.kt
@@ -2,8 +2,11 @@ package com.kkang.jickbangcopy_210107
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.kkang.jickbangcopy_210107.data.Room
 
 class MainActivity : BaseActivity() {
+
+    val mRoomList = ArrayList<Room>()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -18,6 +21,18 @@ class MainActivity : BaseActivity() {
 
     override fun setValues() {
 
+        mRoomList.add(Room(800000000, "서울시 방배동", 4, "서울시 방배동 4층 집입니다."))
+        mRoomList.add(Room(40000000, "서울시 옥수동", 2, "서울시 옥수동 2층 집입니다."))
+        mRoomList.add(Room(6000000, "서울시 용산구", 0, "서울시 용산구 반지하 집입니다."))
+        mRoomList.add(Room(170000000, "서울시 영등포동", 10, "서울시 영등포동 10층 집입니다."))
+        mRoomList.add(Room(2500000, "서울시 동대문구", -1, "동대문구의 지하 1층방 입니다."))
+        mRoomList.add(Room(70000000, "서울시 동대문구", 6, "동대문구의 6층방 입니다."))
+        mRoomList.add(Room(800000000, "서울시 방배동", 4, "서울시 방배동 4층 집입니다."))
+        mRoomList.add(Room(40000000, "서울시 옥수동", 2, "서울시 옥수동 2층 집입니다."))
+        mRoomList.add(Room(6000000, "서울시 용산구", 0, "서울시 용산구 반지하 집입니다."))
+        mRoomList.add(Room(170000000, "서울시 영등포동", 10, "서울시 영등포동 10층 집입니다."))
+        mRoomList.add(Room(2500000, "서울시 동대문구", -1, "동대문구의 지하 1층방 입니다."))
+        mRoomList.add(Room(70000000, "서울시 동대문구", 6, "동대문구의 6층방 입니다."))
     }
 
 }
